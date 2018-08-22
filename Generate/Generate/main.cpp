27e8e1381f97f25dc9a96ce7dc54c4ef55ceeac9
Mridul <mridulsahu01@gmail.com>
2018-08-22 15:23:39 +0530

    Initial Commit

---
+++ Generate/Generate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Generate
+//
+//  Created by Mridul Sahu on 22/08/18.
+//  Copyright © 2018 Open Source. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
